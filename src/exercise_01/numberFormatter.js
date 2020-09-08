7f5a7a303e4391337b4d0575d9b8891661bb569e
BoyuYuan <2608379678@qq.com>
2020-09-08 15:33:06 +0800

    [Boyu Yuan] feat: exercise_01 finished

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -8,5 +8,8 @@ export default function formatNumber(number, option) {
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (option === undefined || option.currency !== true) {
+    return number.toFixed(2);
+  }
+  return `$ ${number.toFixed(2)}`;
 }
