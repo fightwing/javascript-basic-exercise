e8c37edb28c29f9c1422512a84a7a94745867518
BoyuYuan <2608379678@qq.com>
2020-09-08 17:23:28 +0800

    [Boyu Yuan] feat: exercise_04 finished

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,22 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  let node;
+  const head = { value: array[0], next: null };
+  let pnode = head; // 保存上一个节点
+
+  for (let i = 1; i < array.length; i += 1) {
+    node = { value: array[i], next: null };
+    pnode.next = node;
+    pnode = node;
+  }
+  return head;
 }
